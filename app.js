a1d01d4305a1bc989f81aff2f39e938cb0acbd76
Dalia <43527456+dollybuzz@users.noreply.github.com>
2019-11-20 00:47:11 +0000

    added final changes, got random quote from faker dependency to show in header.ejs, accounted for span tag in header.ejs to distinguish current web page title in menu nav, changed server listener to publish to heroku

--- app.js
+++ app.js
@@ -1,34 +1,45 @@
 const express = require("express");
+const faker = require("faker");
 const app = express();
-
-var faker = require("faker");
-var randomSentence = faker.hacker.phrase();
 
 app.engine('html', require('ejs').renderFile);
 app.use(express.static("public"));
 
+//Notes - Changed file names from .html to .ejs, 
+//added in span id tags in header.ejs to distinguish nav menu title color
+//(refer to script tags present in other .ejs files),
+//and passed in new variable from route to view in render lines.
+//The faker is in the header.ejs view as a random quote.
 
 //routes
 app.get("/", function(req, res) {
-    res.render("index.html");
+    res.render("index.ejs", {"randomQuote":faker.hacker.phrase()});
+});
+
+app.get("/index", function(req, res) {
+    res.render("index.ejs", {"randomQuote":faker.hacker.phrase()});
 });
 
 app.get("/howitsused", function(req, res) {
-    res.render("howitsused.html");
+    res.render("howitsused.ejs", {"randomQuote":faker.hacker.phrase()});
 });
 
 app.get("/howitworks", function(req, res) {
-    res.render("howitworks.html");
+    res.render("howitworks.ejs", {"randomQuote":faker.hacker.phrase()});
 });
 
 app.get("/aihistory", function(req, res) {
-    res.render("aihistory.html");
+    res.render("aihistory.ejs", {"randomQuote":faker.hacker.phrase()});
 });
 
 
 //server listener
-app.listen("8080", "127.0.0.1", function() {
+/*app.listen("8080", "127.0.0.1", function() {
+    console.log("Running Express Server...");
+});*/
+
+//accounts for publishing to Heroku
+app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Running Express Server...");
 });
-
 
